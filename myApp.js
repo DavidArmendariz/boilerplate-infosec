b80e62e02ea4a2c507ad5fbe27b7a6ed8548b8c3
David Armendariz <david.adrian.a.p@gmail.com>
2023-05-16 17:08:34 -0500

    Fixed middleware

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
-helmet.hidePoweredBy();
 
 module.exports = app;
 const api = require("./server.js");
+app.use(helmet.hidePoweredBy());
 app.use(express.static("public"));
 app.disable("strict-transport-security");
 app.use("/_api", api);
